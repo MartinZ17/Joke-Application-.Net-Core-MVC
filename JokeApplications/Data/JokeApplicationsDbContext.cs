bedab1c2abc60b6e0a51004af9f5d9cc9b40e53f
user <user@DESKTOP-H8V5E3D>
2023-11-13 12:23:24 +0200

    Add project files.

---
+++ JokeApplications/Data/JokeApplicationsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JokeApplications.Models;
+
+namespace JokeApplications.Data
+{
+    public class JokeApplicationsDbContext : DbContext
+    {
+        public JokeApplicationsDbContext (DbContextOptions<JokeApplicationsDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<JokeApplications.Models.Joke> Joke { get; set; } = default!;
+    }
+}
---
+++ JokeApplications/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JokeApplications.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
